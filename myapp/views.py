aaed5be2ed9c480a4de790882fe82afb422c895f
rahulsingh2310 <rahulkumar.s17@iiits.in>
2018-11-16 15:48:22 +0530

    redefine new category page

--- myapp/views.py
+++ myapp/views.py
@@ -3,8 +3,8 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    return render(request,'myapp/index.html')
+    return render(request,'myapp/category/index.html')
 
 
 def category(request):
-    return render(request,'myapp/category.html')
+    return render(request,'myapp/category/category.html')
